f763769b8b24dc34c6c69264794732f5e9948f0f
Jack Zhuchenko <jack.zhuchenko.stratis@gmail.com>
2019-10-18 23:15:35 +0300

    Start add dynamic to persons list

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,27 +1,70 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 import Header from '../header';
 import RandomPlanet from '../random-planet';
+import ErrorIndicator from '../error-indicator';
+import ErrorButton from '../error-button';
 import ItemList from '../item-list';
 import PersonDetails from '../person-details';
 
 import './app.scss';
 
-const App = () => {
-    return (
-        <div>
-            <Header/>
-            <RandomPlanet/>
+export default class App extends Component {
+    state = {
+        showRandomPlanet: true,
+        selectedPerson: null
+    };
 
-            <div className="row mb2">
-                <div className="col-md-6">
-                    <ItemList/>
+    toggleRandomPlanet = () => {
+        this.setState((state) => {
+            return {
+                showRandomPlanet: !state.showRandomPlanet
+            }
+        });
+    };
+
+    onPersonSelected = (id) => {
+        this.setState({
+            selectedPerson: id,
+        })
+    };
+
+    componentDidCatch() {
+        this.setState({ hasError: true });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <ErrorIndicator />
+        }
+
+        const planet = this.state.showRandomPlanet ?
+            <RandomPlanet/> :
+            null;
+
+        return (
+            <div>
+                <Header/>
+                { planet }
+
+                <div className="row mb2 button-row">
+                    <button
+                        className="toggle-planet btn btn-warning btn-lg"
+                        onClick={this.toggleRandomPlanet}>
+                        Toggle Random Planet
+                    </button>
+                    <ErrorButton />
                 </div>
-                <div className="col-md-6">
-                    <PersonDetails/>
+
+                <div className="row mb2">
+                    <div className="col-md-6">
+                        <ItemList onItemSelected={this.onPersonSelected}/>
+                    </div>
+                    <div className="col-md-6">
+                        <PersonDetails id={this.state.selectedPerson}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
-
-export default App;
--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -11,43 +11,49 @@ export default class RandomPlanet extends Component {
 
     state = {
         planet: {},
-        loading: true,
-        error: false,
+        state: 'loading'
     };
 
-    constructor() {
-        super();
+    componentDidMount() {
         this.updatePlanet();
+        this.interval = setInterval(this.updatePlanet, 10000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
     }
 
     onPlanetLoaded = (planet) => {
         this.setState({
             planet,
-            loading: false,
+            state: 'loaded'
         })
     };
 
     onError = () => {
         this.setState({
-            error: true,
-            loading: false,
+            state: 'error',
         })
     };
 
-    updatePlanet() {
-        const id = Math.floor(Math.random()*25) + 3;
+    updatePlanet = () => {
+        const id = Math.floor(Math.random()*17) + 2;
         this.swapiService
             .getPlanet(id)
             .then(this.onPlanetLoaded)
             .catch(this.onError);
-    }
+    };
 
     render() {
-        const {planet, loading, error} = this.state;
+        const {planet, state} = this.state;
 
         return (
             <div className="random-planet jumbotron rounded">
-                { loading ? <Spinner/> : error ? <ErrorIndicator/> : <PlanetView planet={planet}/> }
+                {{
+                    loading: <Spinner/>,
+                    loaded: <PlanetView planet={planet}/>,
+                    error: <ErrorIndicator/>,
+                }[state]}
             </div>
         )
     }
